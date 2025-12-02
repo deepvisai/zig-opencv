4406a0a83612cc48e2b511eca1c5cbec05fc4dae
Mads Fagerlund <mads@deepvis.ai>
2025-12-02 02:07:59 +0100

    initial commit

---
+++ src/root.zig
@@ -0,0 +1,17 @@
+pub const asyncarray = @import("asyncarray.zig");
+pub const core = @import("core.zig");
+pub const calib3d = @import("calib3d.zig");
+pub const dnn = @import("dnn.zig");
+pub const features2d = @import("features2d.zig");
+pub const highgui = @import("highgui.zig");
+pub const objdetect = @import("objdetect.zig");
+pub const imgcodecs = @import("imgcodecs.zig");
+pub const imgproc = @import("imgproc.zig");
+pub const photo = @import("photo.zig");
+pub const svd = @import("svd.zig");
+pub const version = @import("version.zig");
+pub const videoio = @import("videoio.zig");
+pub const video = @import("video.zig");
+
+pub const c_api = @import("c_api.zig");
+pub const utils = @import("utils.zig");
